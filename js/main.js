0c5b547adf3f9b88f8ac6b4f103959f238f0b66b
Luís Augusto <luisaugusto004mm@outlook.com>
2023-05-22 13:31:27 -0300

    projeto finalizado

--- js/main.js
+++ js/main.js
@@ -1,176 +1,30 @@
-// 6e2e78d98ee9d0c323345799d916c0cc
+let chave = "cebcd482eda57fa9a6714c1c2ba91885"
 
-const apiKey = "6e2e78d98ee9d0c323345799d916c0cc";
-const apiCountryURL = "https://flagsapi.com//flat/64.png";
-const apiUnsplash = "https://source.unsplash.com/1600x900/?";
+function cliqueiNoBotao() {
+  let cidade = document.querySelector(".input-cidade").value
 
-const cityInput = document.querySelector("#city-input");
-const searchBtn = document.querySelector("#search");
-
-const cityElement = document.querySelector("#city");
-const tempElement = document.querySelector("#temperature span");
-const descElement = document.querySelector("#description");
-const weatherIconElement = document.querySelector("#weather-icon");
-const countryElement = document.querySelector("#country");
-const umidityElement = document.querySelector("#umidity span");
-const windElement = document.querySelector("#wind span");
-
-const weatherContainer = document.querySelector("#weather-data");
-
-const errorMessageContainer = document.querySelector("#error-message");
-const loader = document.querySelector("#loader");
-
-const suggestionContainer = document.querySelector("#suggestions");
-const suggestionButtons = document.querySelectorAll("#suggestions button");
-
-// Loader
-const toggleLoader = () => {
-    loader.classList.toggle("hide");
-};
-
-const getWeatherData = async (city) => {
-    toggleLoader();
-
-    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
-
-    const res = await fetch(apiWeatherURL);
-    const data = await res.json();
-
-    toggleLoader();
-
-    return data;
-};
-
-// Tratamento de erro
-const showErrorMessage = () => {
-    errorMessageContainer.classList.remove("hide");
-};
-
-const hideInformation = () => {
-    errorMessageContainer.classList.add("hide");
-    weatherContainer.classList.add("hide");
-
-    suggestionContainer.classList.add("hide");
-};
-
-const showWeatherData = async (city) => {
-    hideInformation();
-
-    const data = await getWeatherData(city);
-
-    if (data.cod === "404") {
-        showErrorMessage();
-        return;
-    }
-
-    cityElement.innerText = data.name;
-    tempElement.innerText = parseInt(data.main.temp);
-    descElement.innerText = data.weather[0].description;
-    weatherIconElement.setAttribute(
-        "src",
-        `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
-    );
-    countryElement.setAttribute("src", apiCountryURL + data.sys.country);
-    umidityElement.innerText = `${data.main.humidity}%`;
-    windElement.innerText = `${data.wind.speed}km/h`;
-
-    // Change bg image
-    document.body.style.backgroundImage = `url("${apiUnsplash + city}")`;
-
-    weatherContainer.classList.remove("hide");
-};
-
-searchBtn.addEventListener("click", async (e) => {
-    e.preventDefault();
-
-    const city = cityInput.value;
-
-    showWeatherData(city);
-});
-
-cityInput.addEventListener("keyup", (e) => {
-    if (e.code === "Enter") {
-        const city = e.target.value;
-
-        showWeatherData(city);
-    }
-});
-
-// Sugestões
-suggestionButtons.forEach((btn) => {
-    btn.addEventListener("click", () => {
-        const city = btn.getAttribute("id");
-
-        showWeatherData(city);
-    });
-});
-
-const searchWrapper = document.querySelector(".search");
-const inputBox = searchWrapper.querySelector("input");
-const sugestBox = searchWrapper.querySelector(".list");
-const icon = searchWrapper.querySelector(".icon");
-let linkTag = searchWrapper.querySelector("a");
-let webLink;
-
-inputBox.onkeyup = (e) => {
-    let userData = e.target.value; //user enetered data
-    let emptyArray = [];
-
-    if (e.key === 'Enter') {
-        if (userData) {
-            window.open(`https://www.google.com/search?q=${userData}`, '_blank')
-        }
-    }
-
-    if (userData) {
-        icon.onclick = () => {
-            webLink = `https://www.google.com/search?q=${userData}`;
-            linkTag.setAttribute("href", webLink);
-            linkTag.click();
-        }
-        emptyArray = suggestions.filter((data) => {
-            //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
-            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
-        });
-        emptyArray = emptyArray.map((data) => {
-            // passing return data inside li tag
-            return data = `<li>${data}</li>`;
-        });
-        searchWrapper.classList.add("active"); //show autocomplete box
-        showSuggestions(emptyArray);
-        let allList = sugestBox.querySelectorAll("li");
-        for (let i = 0; i < allList.length; i++) {
-            //adding onclick attribute in all li tag
-            allList[i].setAttribute("onclick", "select(this)");
-        }
-
-        if (e.key === 'Escape') {
-            searchWrapper.classList.remove("active");
-        }
-    } else {
-        searchWrapper.classList.remove("active"); //hide autocomplete box
-    }
+  buscarCidade(cidade)
 }
 
-function select(element) {
-    let selectData = element.textContent;
-    inputBox.value = selectData;
-    icon.onclick = () => {
-        webLink = `https://www.google.com/search?q=${selectData}`;
-        linkTag.setAttribute("href", webLink);
-        linkTag.click();
-    }
-    searchWrapper.classList.remove("active");
+function colocarNaTela(dados) {
+  console.log(dados)
+  document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+  document.querySelector(".temp").innerHTML = "Temperatura Atual: " + Math.floor(dados.main.temp) + "°C"
+  document.querySelector(".descricao").innerHTML = dados.weather[0].description
+  document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+  document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
+  document.querySelector(".vento").innerHTML = "Velocidade do vento: " + dados.wind.speed + "Km/h"
 }
 
-function showSuggestions(list) {
-    let listData;
-    console.log(!list.length);
-    if (!list.length) {
-        userValue = inputBox.value;
-        listData = `<li>${userValue}</li>`;
-    } else {
-        listData = list.join('');
-    }
-    sugestBox.innerHTML = listData;
+async function buscarCidade(cidade) {
+  let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" +
+    cidade +
+    "&appid=" +
+    chave +
+    "&lang=pt_br" +
+    "&units=metric"
+  )
+    .then(resposta => resposta.json())
+
+  colocarNaTela(dados)
 }
